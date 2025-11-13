db716925e94478bbe4046fc5e31f2da58347106e
felipelovis <vasconceloslovison@gmail.com>
2025-11-13 12:08:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ const PARTICIPANTES = {
 // DATAS LIMITE por fase (CORRIGIDO PARA 2026!)
 const DATAS_LIMITE = {
     'Grupo': new Date('2026-06-11T14:00:00'),
-    '16 avos': new Date('2026-06-28T12:00:00'),
-    'Oitavas de final': new Date('2026-07-02T12:00:00'),
-    'Quartas de final': new Date('2026-07-09T16:00:00'),
-    'Semifinais': new Date('2026-07-14T16:00:00'),
-    'Terceiro e Quarto': new Date('2026-07-18T14:00:00'),
-    'Final': new Date('2026-07-19T16:00:00'),
+    '16 avos': new Date('2025-11-12T12:00:00'),
+    'Oitavas de final': new Date('2025-11-12T12:00:00'),
+    'Quartas de final': new Date('2025-11-12T16:00:00'),
+    'Semifinais': new Date('2025-11-12T16:00:00'),
+    'Terceiro e Quarto': new Date('2025-11-12T14:00:00'),
+    'Final': new Date('2025-11-12T16:00:00'),
 };
 
 // ID DO GOOGLE SHEETS
